441c8d7a8f60f427aba305dd8faa9bd0369746b9
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2015-11-27 11:44:16 +0100

    Initial

---
+++ rm-engine-model/src/main/java/org/lab/rm/engine/model/Item.java
@@ -0,0 +1,47 @@
+package org.lab.rm.engine.model;
+
+import java.math.BigDecimal;
+
+public class Item {
+
+	private Long id;
+
+	private String name;
+
+	private Integer units;
+
+	private BigDecimal weigth;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getUnits() {
+		return units;
+	}
+
+	public void setUnits(Integer units) {
+		this.units = units;
+	}
+
+	public BigDecimal getWeigth() {
+		return weigth;
+	}
+
+	public void setWeigth(BigDecimal weigth) {
+		this.weigth = weigth;
+	}
+
+}
