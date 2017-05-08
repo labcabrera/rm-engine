e6742a62caae4a086440bfefdc2599179f32119f
labcabrera <lab.cabrera@gmail.com>
2017-05-08 21:46:10 +0200

    Initial api

---
+++ rm-engine-api/src/main/java/org/lab/rm/engine/api/RandomController.java
@@ -0,0 +1,12 @@
+package org.lab.rm.engine.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomController {
+
+	public Integer d100() {
+		return 0;
+	}
+
+}
