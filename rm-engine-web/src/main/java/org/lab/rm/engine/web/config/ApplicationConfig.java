8420c4b69a1ccdb13388e0763004f65e341be1cb
labcabrera <lab.cabrera@gmail.com>
2017-05-12 23:14:34 +0200

    Initial web

---
+++ rm-engine-web/src/main/java/org/lab/rm/engine/web/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package org.lab.rm.engine.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.lab.rm.engine.web")
+public class ApplicationConfig {
+
+}
