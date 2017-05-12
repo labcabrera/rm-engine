a20e11246d4f0ed26001fba4488a1be4a5e81013
labcabrera <lab.cabrera@gmail.com>
2017-05-13 01:20:14 +0200

    Initial campaign project + minor web changes

--- rm-engine-web/src/main/java/org/lab/rm/engine/web/config/MvcConfig.java
+++ rm-engine-web/src/main/java/org/lab/rm/engine/web/config/MvcConfig.java
@@ -10,10 +10,12 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/campaign").setViewName("campaign");
 		registry.addViewController("/random").setViewName("random");
 		registry.addViewController("/characters").setViewName("characters");
 		registry.addViewController("/combat").setViewName("combat");
 		registry.addViewController("/movement").setViewName("movement");
+		registry.addViewController("/api").setViewName("api");
 		registry.addViewController("/error").setViewName("error");
 	}
 
