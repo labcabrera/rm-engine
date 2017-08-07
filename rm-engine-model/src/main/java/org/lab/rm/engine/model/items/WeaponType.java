754d0de2a453da70198e28205d37be1258a59340
labcabrera <lab.cabrera@gmail.com>
2017-08-07 14:54:37 +0200

    Upgrade dependencies

--- rm-engine-model/src/main/java/org/lab/rm/engine/model/items/WeaponType.java
+++ rm-engine-model/src/main/java/org/lab/rm/engine/model/items/WeaponType.java
@@ -4,9 +4,11 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Document
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class WeaponType extends Item {
 
 	@Id
